cadf89620c705f902c10d538cf9b4737c053c93f
kelvin <kelvinokoye413@gmail.com>
2023-08-04 15:40:22 +0100

    task1 solutions

---
+++ main_c_folder/task1.c
@@ -0,0 +1,38 @@
+// Write a C program to find the factorial of a non-negative integer entered by the user. The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.
+
+/*
+of a positive number.
+ a factorial ===n!
+ eg
+ 5! = (1*2)*3*4*5
+*/
+
+
+
+
+
+#include <stdio.h>
+
+#define size
+
+int main(void){
+int num;
+printf("Enter a number to determine its factorial : ");
+scanf("%d",&num);
+
+int numbers [size] = {n}
+
+int factorial;
+
+for(int i = n; i < n; i--){
+	factorial += number[i]
+}
+
+printf("the factorial of %d", factorial);
+
+
+
+
+
+return 0;
+}
